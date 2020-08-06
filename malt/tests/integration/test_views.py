30d7b3e71b07d3ccd8eda8e4fb87277006828b1f
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-08-06 12:36:39 -0300

    Fix user view tests

--- malt/tests/integration/test_views.py
+++ malt/tests/integration/test_views.py
@@ -26,88 +26,84 @@ class UserViewTests:
 
     def setUp(self):
         User.objects.create_superuser(self.super_username, password=self.super_password)
-        user = User.objects.create_user(self.username, password=self.password)
-        self.pk = user.pk
+        self.user = User.objects.create_user(self.username, password=self.password)
+
+    def login(self):
+        self.client.login(username=self.username, password=self.password)
 
     def superLogin(self):
         self.client.login(username=self.super_username, password=self.super_password)
 
     def testGetRedirects(self):
-        self.assertEquals(302, self.get_status(kwargs=self.default_kwargs))
+        self.assertEquals(302, self.get_status(kwargs=self.kwargs()))
 
     def testGetForbidsAfterLogin(self):
-        self.client.login(username=self.username, password=self.password)
-        self.assertEquals(403, self.get_status(kwargs=self.default_kwargs))
-
-
-class SpecificUserViewTests(UserViewTests):
-    default_kwargs = {'pk': 0}
-
-    def retrieveUser(self):
-        return User.objects.get(pk=self.pk)
-
-    def specificPost(self, data=None):
-        self.superLogin()
-        self.post(kwargs={'pk': self.pk}, data=data)
-
-    def testGetDoesNotFindAfterSuperLogin(self):
-        self.superLogin()
-        self.assertEquals(404, self.get_status(kwargs=self.default_kwargs))
-
-    def testGet(self):
-        self.superLogin()
-        html = self.get_html(kwargs={'pk': self.pk})
-        h2 = html.select_one('h2')
-        self.assertIn(self.username, h2.string)
-
-    def testPostRedirects(self):
-        self.assertEquals(302, self.post_status(kwargs=self.default_kwargs))
-
-    def testPostForbidsAfterLogin(self):
-        self.client.login(username=self.username, password=self.password)
-        self.assertEquals(403, self.post_status(kwargs=self.default_kwargs))
-
-    def testPostDoesNotFindAfterSuperLogin(self):
-        self.superLogin()
-        self.assertEquals(404, self.post_status(kwargs=self.default_kwargs))
+        self.login()
+        self.assertEquals(403, self.get_status(kwargs=self.kwargs()))
 
 
 class UserManageViewTests(UserViewTests, ViewTestCase):
     view_name = 'user_manage'
-    default_kwargs = None
+
+    def kwargs(self):
+        return None
+
+
+class SpecificUserViewTests(UserViewTests):
+    def kwargs(self):
+        return {'pk': self.user.pk}
+
+    def testGet(self):
+        self.superLogin()
+        html = self.get_html(kwargs=self.kwargs())
+        h2 = html.select_one('h2')
+        self.assertIn(self.username, h2.string)
+
+    def testPostRedirects(self):
+        self.assertEquals(302, self.post_status(kwargs=self.kwargs()))
+
+    def testPostForbidsAfterLogin(self):
+        self.login()
+        self.assertEquals(403, self.post_status(kwargs=self.kwargs()))
+
+    def specificPost(self, data=None):
+        self.superLogin()
+        self.post(kwargs=self.kwargs(), data=data)
 
 
 class UserEditViewTests(SpecificUserViewTests, ViewTestCase):
     view_name = 'user_edit'
 
     def testPost(self):
-        user = self.retrieveUser()
-        user.email = self.email
-        user.first_name = self.first_name
-        user.last_name = self.last_name
-        user.save()
+        self.user.email = self.email
+        self.user.first_name = self.first_name
+        self.user.last_name = self.last_name
+        self.user.save()
 
-        self.assertEquals(self.email, user.email)
-        self.assertEquals(self.first_name, user.first_name)
-        self.assertEquals(self.last_name, user.last_name)
+        self.user.refresh_from_db()
+
+        self.assertEquals(self.email, self.user.email)
+        self.assertEquals(self.first_name, self.user.first_name)
+        self.assertEquals(self.last_name, self.user.last_name)
 
         self.specificPost({
             'email': self.other_email,
             'first_name': self.other_first_name,
             'last_name': self.other_last_name,
         })
-        user.refresh_from_db()
 
-        self.assertEquals(self.other_email, user.email)
-        self.assertEquals(self.other_first_name, user.first_name)
-        self.assertEquals(self.other_last_name, user.last_name)
+        self.user.refresh_from_db()
+
+        self.assertEquals(self.other_email, self.user.email)
+        self.assertEquals(self.other_first_name, self.user.first_name)
+        self.assertEquals(self.other_last_name, self.user.last_name)
 
 
 class UserRemoveViewTests(SpecificUserViewTests, ViewTestCase):
     view_name = 'user_remove'
 
     def exists(self):
-        return User.objects.filter(pk=self.pk).exists()
+        return User.objects.filter(pk=self.user.pk).exists()
 
     def assertExists(self):
         self.assertTrue(self.exists())
@@ -127,47 +123,43 @@ class UserRemoveViewTests(SpecificUserViewTests, ViewTestCase):
 
 
 class UserChangeViewTests(SpecificUserViewTests):
-    def power(self, user):
-        return PowerUser.objects.filter(user=user).exists()
+    def power(self):
+        return PowerUser.objects.filter(user=self.user).exists()
 
-    def assertPower(self, user):
-        self.assertTrue(self.power(user))
-        self.assertTrue(power_cache.get(user))
+    def assertPower(self):
+        self.assertTrue(self.power())
+        self.assertTrue(power_cache.get(self.user))
 
-    def assertNotPower(self, user):
-        self.assertFalse(self.power(user))
-        self.assertFalse(power_cache.get(user))
+    def assertNotPower(self):
+        self.assertFalse(self.power())
+        self.assertFalse(power_cache.get(self.user))
 
 
 class UserPromoteViewTests(UserChangeViewTests, ViewTestCase):
     view_name = 'user_promote'
 
     def testPost(self):
-        user = self.retrieveUser()
-        self.assertNotPower(user)
+        self.assertNotPower()
         self.specificPost()
-        self.assertPower(user)
+        self.assertPower()
 
     def testIdempotence(self):
-        user = self.retrieveUser()
         self.specificPost()
         self.specificPost()
-        self.assertPower(user)
+        self.assertPower()
 
 
 class UserDemoteViewTests(UserChangeViewTests, ViewTestCase):
     view_name = 'user_demote'
 
     def testPost(self):
-        user = self.retrieveUser()
-        PowerUser.objects.create(user=user)
-        self.assertPower(user)
+        PowerUser.objects.create(user=self.user)
+        self.assertPower()
         self.specificPost()
-        self.assertNotPower(user)
+        self.assertNotPower()
 
     def testIdempotence(self):
-        user = self.retrieveUser()
-        PowerUser.objects.create(user=user)
+        PowerUser.objects.create(user=self.user)
         self.specificPost()
         self.specificPost()
-        self.assertNotPower(user)
+        self.assertNotPower()
