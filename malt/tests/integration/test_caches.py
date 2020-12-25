6b7f8d17bb4f205dd7b0fae4d14699e26967d428
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-12-25 20:31:21 -0300

    Refactor cache tests

--- malt/tests/integration/test_caches.py
+++ malt/tests/integration/test_caches.py
@@ -9,47 +9,131 @@ User = get_user_model()
 
 
 class PowerCacheTests(IntegrationTestCase):
-    username = 'u'
-    other_username = 'ou'
-
     def setUp(self):
-        self.user = self.create(self.username)
-
-    def create(self, username):
-        return User.objects.create_user(username)
+        self.user = User.objects.create_user('u')
 
     def promote(self):
         PowerUser.objects.create(user=self.user)
 
+    def demote(self):
+        PowerUser.objects.filter(user=self.user).delete()
+
+    def get(self):
+        return power_cache.get(self.user)
+
+    def set(self, value):
+        power_cache.set(self.user, value)
+
     def assertPower(self):
-        self.assertTrue(power_cache.get(self.user))
+        self.assertTrue(self.get())
 
     def assertNotPower(self):
-        self.assertFalse(power_cache.get(self.user))
+        self.assertFalse(self.get())
 
-    def testNotPowerBeforePromote(self):
+    def testNotPower(self):
         self.assertNotPower()
 
-    def testPowerBeforePromoteIfCached(self):
-        power_cache.set(self.user, True)
+    def testNotPowerAfterGet(self):
+        self.get()
+        self.assertNotPower()
+
+    def testNotPowerAfterSetFalse(self):
+        self.set(False)
+        self.assertNotPower()
+
+    def testPowerAfterSetTrue(self):
+        self.set(True)
         self.assertPower()
-
-    def testNotPowerBeforePromoteIfOtherCached(self):
-        other_user = self.create(self.other_username)
-        power_cache.set(other_user, True)
-        self.assertNotPower()
 
     def testPowerAfterPromote(self):
         self.promote()
         self.assertPower()
 
-    def testNotPowerAfterPromoteIfCached(self):
-        power_cache.set(self.user, False)
+    def testNotPowerAfterGetPromote(self):
+        self.get()
         self.promote()
         self.assertNotPower()
 
-    def testPowerAfterPromoteIfOtherCached(self):
-        other_user = self.create(self.other_username)
-        power_cache.set(other_user, False)
+    def testPowerAfterPromoteGet(self):
         self.promote()
+        self.get()
+        self.assertPower()
+
+    def testNotPowerAfterSetFalsePromote(self):
+        self.set(False)
+        self.promote()
+        self.assertNotPower()
+
+    def testNotPowerAfterPromoteSetFalse(self):
+        self.promote()
+        self.set(False)
+        self.assertNotPower()
+
+    def testPowerAfterSetTruePromote(self):
+        self.set(True)
+        self.promote()
+        self.assertPower()
+
+    def testPowerAfterPromoteSetTrue(self):
+        self.promote()
+        self.set(True)
+        self.assertPower()
+
+    def testNotPowerAfterPromoteDemote(self):
+        self.promote()
+        self.demote()
+        self.assertNotPower()
+
+    def testNotPowerAfterGetPromoteDemote(self):
+        self.get()
+        self.promote()
+        self.demote()
+        self.assertNotPower()
+
+    def testPowerAfterPromoteGetDemote(self):
+        self.promote()
+        self.get()
+        self.demote()
+        self.assertPower()
+
+    def testNotPowerAfterPromoteDemoteGet(self):
+        self.promote()
+        self.demote()
+        self.get()
+        self.assertNotPower()
+
+    def testNotPowerAfterSetFalsePromoteDemote(self):
+        self.set(False)
+        self.promote()
+        self.demote()
+        self.assertNotPower()
+
+    def testNotPowerAfterPromoteSetFalseDemote(self):
+        self.promote()
+        self.set(False)
+        self.demote()
+        self.assertNotPower()
+
+    def testNotPowerAfterPromoteDemoteSetFalse(self):
+        self.promote()
+        self.demote()
+        self.set(False)
+        self.assertNotPower()
+
+    def testPowerAfterSetTruePromoteDemote(self):
+        self.set(True)
+        self.promote()
+        self.demote()
+        self.assertPower()
+
+    def testPowerAfterPromoteSetTrueDemote(self):
+        self.promote()
+        self.set(True)
+        self.demote()
+        self.assertPower()
+
+    def testPowerAfterPromoteDemoteSetTrue(self):
+        self.promote()
+        self.demote()
+        self.set(True)
         self.assertPower()
