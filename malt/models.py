0d4bec9e7276ece3d3b32ef9cbe956d3e30b4b56
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-12-30 19:23:36 -0300

    Fix file asset model

--- malt/models.py
+++ malt/models.py
@@ -33,15 +33,15 @@ class FolderAsset(Asset):
 
 
 class FileAssetManager(models.Manager):
-    @transaction.atomic
-    def create(self, **kwargs):
+    def get_or_create(self, **kwargs):
         if 'uid' in kwargs:
             raise IntegrityError('uid not allowed in kwargs')
         user = kwargs.pop('user', None)
-        while True:
-            uid = uuid()
-            if not self.filter(user=user, uid=uid).exists():
-                return super().create(user=user, uid=uid, **kwargs)
+        with transaction.atomic():
+            while True:
+                uid = uuid()
+                if not self.filter(user=user, uid=uid).exists():
+                    return super().get_or_create(user=user, uid=uid, **kwargs)
 
 
 class FileAsset(Asset):
